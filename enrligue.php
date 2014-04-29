b1b94251e3183566ded13adf4a6cdb4b0ae586b9
Eloi <eloipagniez@gmail.com>
2014-04-29 14:52:00 +0200

    6

--- enrligue.php
+++ enrligue.php
@@ -12,8 +12,17 @@
     $sigle = $_POST["sigle"];
     $president = $_POST["president"];
     
-    $SQL = "INSERT INTO ligues(numero, nom, sigle, president) VALUES('".$numero."', '".$nom."', '".$sigle."', '".$president."')";
+    $SQL = "SELECT numero FROM ligues";
+    $rs=$cnx->query($SQL);
     
-   $rs=$cnx->query($SQL);
+    while($info=$rs->fetch_object()) {
+        if($info->numero == $numero) {
+            echo 'existant';
+            exit;
+        }
+    }
+    
+   $SQL2 = "INSERT INTO ligues(numero, nom, sigle, president) VALUES('".$numero."', '".$nom."', '".$sigle."', '".$president."')";
+   $rs2=$cnx->query($SQL2);
    echo 'ok';
 ?>
